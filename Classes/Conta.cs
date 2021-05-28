88d722a360e2182b4e37867beef6c79467f33b4c
flavianventura <flavianventura@gmail.com>
2021-05-28 11:28:58 -0300

    Projeto do bootcamp .Net fundamentals - Criando uma aplicação de transferências bancárias com .NET

---
+++ Classes/Conta.cs
@@ -0,0 +1,58 @@
+using System;
+
+namespace DIO.Bank
+{
+    public class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+
+        public bool Sacar(double valorSacado)
+        {
+            double creditoTotal = this.Saldo + this.Credito;
+
+            if(creditoTotal - valorSacado >= 0)
+            {
+                this.Saldo -= valorSacado;
+                return true;
+            }else{
+                Console.WriteLine("Saldo insufuciente!");
+                return false;
+            }
+        }
+
+        public void Depositar(double valorDeposito)
+		{
+			this.Saldo += valorDeposito;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+		}
+
+		public void Transferir(double valorTransferencia, Conta contaDestino)
+		{
+			if (this.Sacar(valorTransferencia)){
+                contaDestino.Depositar(valorTransferencia);
+            }
+		}
+
+        public override string ToString()
+		{
+            string retorno = "";
+            retorno += "TipoConta " + this.TipoConta + " | ";
+            retorno += "Nome " + this.Nome + " | ";
+            retorno += "Saldo " + this.Saldo + " | ";
+            retorno += "Crédito " + this.Credito;
+			return retorno;
+		}
+    }
+}
